e2d176244620d8796df6f44b13601085c841c166
DevEmanoel <emsb3@aluno.ifal.edu.br>
2024-07-25 02:22:33 -0300

    criar primeiro comando do bot de estudos

---
+++ src/discord/commands/public/ping.ts
@@ -0,0 +1,29 @@
+import { Command } from "#base";
+import { settings } from "#settings"
+import { createEmbed } from "@magicyan/discord";
+import { ApplicationCommandType } from "discord.js";
+
+new Command({
+	name: "ping",
+	description: "Teste de ping",
+	dmPermission: false,
+	type: ApplicationCommandType.ChatInput,
+	async run(interaction) {
+		const Author = interaction.user;
+		const ping = interaction.client.ws.ping;
+
+		const embed = createEmbed({
+			color: settings.colors.magic,
+			description: `Salve meu mano ${Author.username}! Tá aqui seu teste de ping \`loading...\``
+		});
+
+		await interaction.reply({ embeds: [embed], ephemeral }).then(() => {
+			const embed02 = createEmbed({
+				color: settings.colors.azoxo,
+				description: `Salve meu mano ${Author.username}! Tá aqui seu teste de ping \`${ping}\``
+			});
+
+			interaction.editReply({ embeds: [embed02] });
+		});
+	}
+})
